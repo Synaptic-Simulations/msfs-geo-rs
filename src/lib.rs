46107c5b81007b426c5cddc20a6fe5f47c5bad81
Alex Cutforth <alex@densem.net>
2022-06-03 22:11:08 +1200

    feat: bearing_intersection

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,15 @@ pub mod constants;
 mod distance_bounds;
 mod distance_to;
 pub mod macros;
+mod place_bearing_intersection;
 pub mod utility;
 
-use uom::si::{angle::degree, f64::Angle};
+use nalgebra::Matrix1x3;
+use uom::si::{
+	angle::{degree, radian},
+	f64::Angle,
+	ratio::ratio,
+};
 
 #[derive(PartialEq, Eq)]
 pub enum Direction {
@@ -15,6 +21,7 @@ pub enum Direction {
 	Either,
 }
 
+#[derive(Copy, Clone, Default, Debug)]
 /// Coordinates represent a location on the surface of a sphere using angular distance from its equator and prime
 /// meridian
 pub struct Coordinates {
@@ -31,5 +38,26 @@ impl Coordinates {
 			lat: Angle::new::<degree>(lat),
 			long: Angle::new::<degree>(long),
 		}
+	}
+}
+
+pub type Spherical = Matrix1x3<f64>;
+
+impl From<Spherical> for Coordinates {
+	fn from(spherical: Spherical) -> Self {
+		Self {
+			lat: Angle::new::<radian>(spherical[2].asin()),
+			long: Angle::new::<radian>(spherical[1].atan2(spherical[0])),
+		}
+	}
+}
+
+impl Into<Spherical> for Coordinates {
+	fn into(self) -> Spherical {
+		Matrix1x3::new(
+			self.lat.cos().get::<ratio>() * self.long.cos().get::<ratio>(),
+			self.lat.cos().get::<ratio>() * self.long.sin().get::<ratio>(),
+			self.lat.sin().get::<ratio>(),
+		)
 	}
 }
---
+++ src/place_bearing_intersection.rs
@@ -0,0 +1,102 @@
+use std::marker::PhantomData;
+
+use uom::si::f64::{Angle, Length};
+
+use crate::{utility::clamp_angle_cw, Coordinates, Spherical};
+
+const INTERMEDIATE_PLACE_DISTANCE: Length = Length {
+	units: PhantomData,
+	dimension: PhantomData,
+	value: 926000.0,
+};
+
+impl Coordinates {
+	/// Calculates the two intercept Coordinates of two great circles running through `self` and `place2` respectively,
+	/// and on bearings `bearing` and `bearing2` respectively. **Note** that the great circles only have the given
+	/// bearings at the given Coordinates
+	pub fn place_bearing_intersection(
+		self, bearing: Angle, place2: Coordinates, bearing2: Angle,
+	) -> (Coordinates, Coordinates) {
+		let pa11: Spherical = self.into();
+		let pa12: Spherical = self.bearing_distance(bearing, INTERMEDIATE_PLACE_DISTANCE).into();
+		let pa21: Spherical = place2.into();
+		let pa22: Spherical = place2.bearing_distance(bearing2, INTERMEDIATE_PLACE_DISTANCE).into();
+
+		let n1 = pa11.cross(&pa12);
+		let n2 = pa21.cross(&pa22);
+
+		let l = n1.cross(&n2);
+		let i = l.norm();
+
+		let i1 = l / i;
+		let i2 = i1 * -1.0;
+
+		let s1 = Coordinates::from(i1);
+		let s2 = Coordinates::from(i2);
+
+		let brg_to_s1 = self.bearing_to(s1);
+		let brg_to_s2 = self.bearing_to(s2);
+
+		let delta1 = (clamp_angle_cw(bearing) - brg_to_s1).abs();
+		let delta2 = (clamp_angle_cw(bearing) - brg_to_s2).abs();
+
+		if delta1 < delta2 {
+			(s1, s2)
+		} else {
+			(s2, s1)
+		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use uom::{
+		si::angle::{degree, radian},
+		ConstZero,
+	};
+
+	use super::*;
+	use crate::assert_uom_eq;
+
+	#[test]
+	fn test_place_bearing_intersection_poles() {
+		let points = Coordinates::new(39.778889, -104.9825).place_bearing_intersection(
+			Angle::ZERO,
+			Coordinates::new(43.778889, -102.9825),
+			Angle::ZERO,
+		);
+		assert_uom_eq!(points.0.lat, Angle::HALF_TURN / 2.0);
+		assert_uom_eq!(points.1.lat, Angle::HALF_TURN / -2.0);
+
+		let points = Coordinates::new(39.778889, -104.9825).place_bearing_intersection(
+			Angle::HALF_TURN,
+			Coordinates::new(43.778889, -102.9825),
+			Angle::HALF_TURN,
+		);
+		assert_uom_eq!(points.0.lat, Angle::HALF_TURN / -2.0);
+		assert_uom_eq!(points.1.lat, Angle::HALF_TURN / 2.0);
+	}
+
+	#[test]
+	fn test_place_bearing_intersection_equator() {
+		let points = Coordinates::new(43.0, -104.9825).place_bearing_intersection(
+			Angle::new::<degree>(175.0),
+			Coordinates::new(-43.0, -104.9825),
+			Angle::new::<degree>(5.0),
+		);
+		assert_uom_eq!(points.0.lat, Angle::ZERO);
+		assert_uom_eq!(points.1.lat, Angle::ZERO);
+	}
+
+	#[test]
+	fn test_place_bearing_intersection_triangle() {
+		let points = Coordinates::new(-43.0, 0.0).place_bearing_intersection(
+			Angle::HALF_TURN / -4.0,
+			Coordinates::new(-43.0, -90.0),
+			Angle::HALF_TURN / 4.0,
+		);
+		assert_uom_eq!(points.0.lat, Angle::new::<radian>(0.29828558585826787));
+		assert_uom_eq!(points.0.long, Angle::HALF_TURN / -4.0);
+		assert_uom_eq!(points.1.long, Angle::FULL_TURN / (8.0 / 3.0));
+	}
+}
